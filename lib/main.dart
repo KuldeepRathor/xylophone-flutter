7f582971e8ff42a3edc4f6e467431735c0b8e62a
TheMuellenator <themuellenator@users.noreply.github.com>
2020-01-17 18:57:01 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3 Adding AndroidX gradle properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp
+    (
+      XylophoneApp()
+  );
+}
 
 class XylophoneApp extends StatelessWidget {
+
+  void playsound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+   Expanded buildKey({Color color, int soundNumber}){
+    return Expanded(
+      child: InkWell(
+        splashColor: color,
+        onTap: (){
+          playsound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.blue, soundNumber: 5),
+              buildKey(color: Colors.indigo, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
